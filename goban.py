d71619d3ab8b1acd91aa09686fde7986e4baecf8
Thomas Magalhaes <thomas.magalhaes.dev@gmail.com>
2019-11-20 14:15:35 +0100

    goban: implement is_taken method

--- goban.py
+++ goban.py
@@ -42,5 +42,44 @@ class Goban(object):
         elif self.goban[y][x] == "#":
             return Status.BLACK
 
-    def is_taken(self, x, y):
-        pass
+    def is_taken(self, x, y, ignore_list=None, base_status=None):
+        """
+        Tells if a given position is taken or not
+
+        Args:
+            x: the x coordinate
+            y: the y coordinate
+            ignore_list: the set of positions (as tuples) to ignore when
+            looking at adjacent positions (default is None)
+            base_status: the status of targetted shape (default is None)
+
+        Returns:
+            True if the given position is taken, False otherwise
+        """
+        if ignore_list is None:
+            ignore_list = set()
+        ignore_list.add((x, y))
+
+        status = self.get_status(x, y)
+        if status == Status.EMPTY:
+            return False
+        elif status == Status.OUT:
+            return base_status is not None
+        elif base_status is None:
+            base_status = status
+        elif status != base_status:
+            return True
+
+        taken_count = 0
+        for side in [(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)]:
+            if side in ignore_list:
+                continue
+
+            taken_count += 1
+            if not self.is_taken(
+                *side, ignore_list=ignore_list, base_status=base_status
+            ):
+                return False
+            taken_count -= 1
+
+        return taken_count == 0
