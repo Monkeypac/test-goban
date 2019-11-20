17f2c6de1109b8b9b2f3141ed5bc147e1a505f14
Thomas Magalhaes <thomas.magalhaes.dev@gmail.com>
2019-11-20 14:14:48 +0100

    tests: add a few tests to check edge cases

--- test_goban.py
+++ test_goban.py
@@ -60,3 +60,69 @@ def test_square_shape_is_taken():
     assert goban.is_taken(0, 2) is True
     assert goban.is_taken(1, 1) is True
     assert goban.is_taken(1, 2) is True
+
+def test_full_shape_is_taken():
+    goban = Goban([
+        '###',
+        '###',
+        '###',
+    ])
+
+    assert goban.is_taken(0, 0) is True
+    assert goban.is_taken(0, 1) is True
+    assert goban.is_taken(0, 2) is True
+    assert goban.is_taken(1, 0) is True
+    assert goban.is_taken(1, 1) is True
+    assert goban.is_taken(1, 2) is True
+    assert goban.is_taken(2, 0) is True
+    assert goban.is_taken(2, 1) is True
+    assert goban.is_taken(2, 2) is True
+
+def test_two_shapes_one_taken_one_not():
+    goban = Goban([
+        '#o.',
+        'o#.',
+        '...',
+    ])
+
+    assert goban.is_taken(0, 0) is True
+    assert goban.is_taken(1, 1) is False
+
+def test_circle():
+    goban = Goban([
+        '###',
+        '#.#',
+        '###',
+    ])
+
+    assert goban.is_taken(0, 0) is False
+    assert goban.is_taken(1, 1) is False
+
+def test_circle_with_whites():
+    goban = Goban([
+        'ooooo',
+        'o###o',
+        'o#.#o',
+        'o###o',
+        'ooooo'
+    ])
+
+    assert goban.is_taken(0, 0) is True
+    assert goban.is_taken(1, 1) is False
+    assert goban.is_taken(2, 2) is False
+
+def test_empty_not_taken():
+    goban = Goban([
+        '.'
+    ])
+
+    assert goban.is_taken(0, 0) is False
+
+def test_diagonals_do_not_count():
+    goban = Goban([
+        'o.o',
+        '.x.',
+        'o.o',
+    ])
+
+    assert goban.is_taken(1, 1) is False
